7adc644462b2866bb250ba3cc2c47e12089b2b2f
jcert <jgcertorio@uol.com.br>
2016-09-02 17:24:21 -0300

    init

---
+++ python/main.py
@@ -0,0 +1,20 @@
+
+#pin def
+
+	#motor pins
+		#motor A
+		#motor B
+
+	#encoder pins
+		#encoder A
+		#encoder B
+
+	#make thread to poll the encoders
+	#make while loop to get direction commands
+		#eventually make a control system using both, so you just give global/local positions
+
+#
+
+
+
+
